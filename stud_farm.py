78269c6397d5574ce30f1057a4cff97fd0bff33c
Sam Holt <samueljrholt@gmail.com>
2018-12-19 17:12:12 +0000

    Working on CSV to XML conversion

--- stud_farm.py
+++ stud_farm.py
@@ -4,6 +4,7 @@
 import argparse
 import subprocess
 import re
+import csv
 
 # Define and parse options
 def parse_opts():
@@ -22,55 +23,94 @@ def parse_opts():
                         help="Make XML and quit")
 
     args = parser.parse_args()
-    print(parser.help)
     return args
+
 
 # Validate credentials
 def validate_credentials(username, password, submit):
     # Build curl command:
     c = "curl"
     u = "-u"
-    d =  (username + ":" + password)
+
+    try:
+        d =  (username + ":" + password)
+    except:
+        print("ERROR: Username or password is null, cannot validate submission")
+        return False
+
     F = "-F SUBMISSION=@validate.xml"
+
     if submit:
         URL = "https://wwwdev.ebi.ac.uk/ena/submit/drop-box/submit/"
     else:
         URL = "https://www.ebi.ac.uk/ena/submit/drop-box/submit/"
 
-    # Run curl validation command, just checks that credentials work
+    # Submission command with validation XML to check credential validity
     credential_check = subprocess.check_output([c, u, d, F, URL],
                                                universal_newlines=True)
 
-    if re.search("success=\"true\"", credential_check):
+    # Handle result of check
+    if re.search("Access Denied", credential_check):
+        print("Invalid username/password")
+        return False
+    else:
         print("Credentials validated")
         return True
-    else:
-        print("Invalid username/password")
-        quit()
 
-    # see curlthing.py
-    # return true or false
 
 # Parse input CSV into list of rows
-def get_rows():
-    pass
+def get_rows(input_csv):
+    tsv_rows = csv.reader(open(input_csv, 'r'), delimiter='\t')
+    return tsv_rows
     # see tsv2xml.py
+
 
 # Validate input spreadsheet content
-def validate_csv_row():
-    pass
+def validate_csv_row(row):
+    #TODO: some actual validation
+    return True
     # write and call subfunctions to validate each element
 
+
+# Convert row from input TSV to study set XML
+def csv_to_xml(row):
+    return """
+    <PROJECT alias="%s">
+        <NAME>%s</NAME>
+        <TITLE>%s</TITLE>
+        <DESCRIPTION>%s</DESCRIPTION>
+        <SUBMISSION_PROJECT>
+            <SEQUENCING_PROJECT/>
+        </SUBMISSION_PROJECT>
+        <PROJECT_LINKS>
+           <PROJECT_LINK>
+              <XREF_LINK>
+                 <DB>PUBMED</DB>
+                 <ID>%s</ID>
+              </XREF_LINK>
+           </PROJECT_LINK>
+        </PROJECT_LINKS>
+    </PROJECT>""" % (
+    row[0], row[1], row[2], row[3], row[4])
+
 # Generate Project Set XML
-def generate_study_xml():
-    pass
-    # see tsv2xml.py
-    # will call validate_csv_row once written
-    validate_csv_row()
+def generate_study_xml(input_csv, validate, generate_xml):
+    csv_rows = get_rows(input_csv)
+
+    xml = open("study_set.xml", "w")
+    xml.write("<PROJECT_SET>")
+
+    for row in csv_rows:
+        validate_csv_row(row)
+        csv_to_xml(row)
+
+    xml.write("\n</PROJECT_SET>")
+    xml.close()
 
 # Generate Submission instruction XML
 def generate_submission_xml():
     pass
+
 
 # Generate Validate instruction XML
 def generate_validate_xml():
@@ -85,12 +125,16 @@ def generate_validate_xml():
 
     with open("validate.xml", "w") as validate_xml:
         validate_xml.write(validate_instruction)
+        return True
+
+    return False
 
 
 # Validate
 def curl_validate():
     pass
     # Run submission with validation XML, use to decide whether to proceed
+
 
 # Submit to ENA
 def curl_submit():
@@ -99,11 +143,13 @@ def curl_submit():
     # Include warning if TEST
     # Capture output
 
+
 # Report Accessions
 def report_accessions():
     pass
     # parse result of curl_submit, report accessions
     # Repeat TEST warning
+
 
 # Error reporting along the way
 
@@ -111,13 +157,24 @@ def report_accessions():
 def __main__():
 
     user_args = parse_opts()
+    print(user_args.validate)
 
-    generate_validate_xml()
+    try:
+        generate_validate_xml()
+    except:
+        print("Could not generate validation XML, exiting")
+        quit()
 
-    validate_credentials(user_args.username, user_args.password,
-                         user_args.submit)
-
-    get_rows()
+    if (user_args.generate_xml):
+        pass
+    else:
+        if validate_credentials(user_args.username, user_args.password,
+                             user_args.submit):
+            pass
+        else:
+            print("Credential validation failed, exiting program")
+    
+    generate_study_xml(user_args.input_csv, user_args.validate, user_args.generate_xml)
 
     generate_study_xml()
 
