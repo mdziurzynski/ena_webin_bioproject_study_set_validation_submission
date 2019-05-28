2b6cd5f9262a8c107588d5ed08b7fd8515fe3edb
Mikolaj Dziurzynski <dziurzynskim@gmail.com>
2019-05-28 22:45:09 +0200

    Small fixes for tests

--- tests/test_csv_to_xml.py
+++ tests/test_csv_to_xml.py
@@ -3,14 +3,18 @@ from study_subs import csv_to_xml
 import csv
 import xml.etree.ElementTree as ElementTree
 from lxml import etree
+import os
 
 
 class Test_csv_to_xml(unittest.TestCase):
 
     def setUp(self):
-        csv_rows = csv.reader(open("tests\studySubmissionData.tsv", 'r'), delimiter='\t')
-        next(csv_rows) # Skips header row
-        self.row = next(csv_rows)
+        test_data_path = os.path.join('tests', 'studySubmissionData.tsv')
+
+        with open(test_data_path, 'r') as tsv_file:
+            csv_rows = csv.reader(tsv_file, delimiter='\t')
+            next(csv_rows) # Skips header row
+            self.row = next(csv_rows)
 
     def test_csv_to_xml_correct(self):
         csv2xml = csv_to_xml(self.row)
